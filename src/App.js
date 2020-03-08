ffe275d4fce90046db224e9c8e4c95241658406f
smjeong88 <smvtr123@naver.com>
2020-03-08 14:46:54 +0900

    components add title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import Title from './components/title'
+
+// state : 내가 가진 값
+// props : 누군가를 통해 받은 값
+
+// export default : 하나만 내보낼때
+// export : 여러개
 
 function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <Title text="메인 페이지" />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/title.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import PropTypes from 'PropTypes'
+
+const TitleContainer = styled.div`
+  font-size: 31px;
+  font-weight: bold;
+  padding: 20px 10px;
+  border-bottom: 1px solid #efefef;
+`
+
+function Title({ text }) {
+  //const [title, setTitle] = useState('리액트공부') //state
+
+  return <TitleContainer>{text}</TitleContainer>
+}
+
+Title.propTypes = {
+  text: PropTypes.string,
+}
+
+export default Title
